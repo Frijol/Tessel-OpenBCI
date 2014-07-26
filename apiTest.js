aa82e6f9877dddd4da8a077e3dfab1b1eed3b718
Frijol <ifoundthemeaningoflife@gmail.com>
2014-07-25 20:00:57 -0700

    ok, looks like a module test. now for actual functionality.

--- apiTest.js
+++ apiTest.js
@@ -1,5 +1,5 @@
 // Checks API calls to make sure they work
-var tinytap = require('tinytap');
+var test = require('tinytap');
 var async = require('async');
 
 var tessel = require('tessel');
@@ -7,12 +7,18 @@ var portname = process.argv[2] || 'A';
 var BCILib = require('./');
 var BCI = BCILib.use(tessel.port[portname]);
 
-BCI._reset(function () {
-  console.log('reset')
-  BCI._start(function () {
-    console.log('started')
-    BCI._rdatac(function () {
-      console.log('did something')
+async.series([
+  
+  test('Connecting', function (t) {
+    BCI._reset(function () {
+      console.log('reset');
+      BCI._start(function () {
+        console.log('started');
+        BCI._rdatac(function () {
+          console.log('sent command to read data continuously');
+        });
+      });
     });
-  });
-});
+  })
+
+]);
