81d89ea80d01ae705fbca33c3d478617c2dfb8ca
Frijol <ifoundthemeaningoflife@gmail.com>
2014-07-25 19:45:42 -0700

    actually treating SPI right now

--- index.js
+++ index.js
@@ -115,17 +115,17 @@ function BCI(hardware, callback) {
   // });
 }
 
-// We want the ability to emit events
-util.inherits(BCI, EventEmitter);
+// // We want the ability to emit events
+// util.inherits(BCI, EventEmitter);
 
 BCI.prototype._wakeUp = function (callback) {
   // only allowed to send WAKEUP after sending STANDBY
   this.chipSelect.output(false);
-  this.spi.send(_WAKEUP, function (err) {
+  this.spi.transfer(new Buffer([_WAKEUP]), function (err, rxbuf) {
     setTimeout(function () {
       this.chipSelect.output(true);
       if (callback) {
-        callback();
+        callback(rxbuf);
       }
     }, 3); //must wait 4 tCLK cycles before sending another command (Datasheet, pg. 35)
   });
@@ -133,22 +133,23 @@ BCI.prototype._wakeUp = function (callback) {
 
 BCI.prototype._standby = function (callback) {
   this.chipSelect.output(false);
-  this.spi.send(_STANDBY, function (err) {
+  this.spi.transfer(new Buffer([_STANDBY]), function (err, rxbuf) {
     this.chipSelect.output(true);
     if (callback) {
-      callback();
+      callback(rxbuf);
     }
   });
 };
 
 // Reset all the registers to default settings
 BCI.prototype._reset = function (callback) {
-  this.chipSelect.output(false);
-  this.spi.send(_RESET, function (err) {
+  var self = this;
+  self.chipSelect.output(false);
+  self.spi.transfer(new Buffer([_RESET]), function (err, rxbuf) {
     setTimeout(function () {
-      this.chipSelect.output(true);
+      self.chipSelect.output(true);
       if (callback) {
-        callback();
+        callback(rxbuf);
       }
     }, 12); //must wait 18 tCLK cycles to execute this command (Datasheet, pg. 35)
   });
@@ -157,10 +158,10 @@ BCI.prototype._reset = function (callback) {
 // Start data conversion
 BCI.prototype._start = function (callback) {
   this.chipSelect.output(false);
-  this.spi.send(_START, function (err) {
+  this.spi.transfer(new Buffer([_START]), function (err, rxbuf) {
     this.chipSelect.output(true);
     if (callback) {
-      callback();
+      callback(rxbuf);
     }
   });
 };
@@ -168,22 +169,23 @@ BCI.prototype._start = function (callback) {
 // Stop data conversion
 BCI.prototype._stop = function (callback) {
   this.chipSelect.output(false);
-  this.spi.send(_STOP, function (err) {
+  this.spi.transfer(new Buffer([_STOP]), function (err, rxbuf) {
     this.chipSelect.output(true);
     if (callback) {
-      callback();
+      callback(rxbuf);
     }
   });
 };
 
 // Read data continuously
 BCI.prototype._rdatac = function (callback) {
-  this.chipSelect.output(false);
-  this.spi.send(_RDATAC, function (err) {
+  var self = this;
+  self.chipSelect.output(false);
+  self.spi.transfer(new Buffer([_RDATAC]), function (err, rxbuf) {
     setTimeout(function () {
-      this.chipSelect.output(true);
+      self.chipSelect.output(true);
       if (callback) {
-        callback();
+        callback(rxbuf);
       }
     }, 3);
   });
@@ -191,17 +193,318 @@ BCI.prototype._rdatac = function (callback) {
 
 // Send data continuously
 BCI.prototype._sdatac = function (callback) {
-  this.chipSelect.output(false);
-  this._send(_SDATAC, function (err) {
+  var self = this;
+  self.chipSelect.output(false);
+  self.spi.transfer(new Buffer([_SDATAC]), function (err, rxbuf) {
     setTimeout(function () {
-      this.chipSelect.output(true);
+      self.chipSelect.output(true);
       if (callback) {
-        callback();
+        callback(rxbuf);
       }
     }, 3); //must wait 4 tCLK cycles after executing this command (Datasheet, pg. 37)
   });
 };
 
+// BCI.prototype._getDeviceID = function (callback) {
+//   // byte ADS1299::getDeviceID() {			// simple hello world com check
+//   //   byte data = RREG(0x00);
+//   //   if(verbose){						// verbose otuput
+//   //     Serial.print(F("Device ID "));
+//   //     printHex(data);	
+//   //   }
+//   //   return data;
+//   // }
+// }
+// 
+// BCI.prototype._readOneRegister = function (address, callback) {
+//   var opcode1 = 
+// }
+
+
+/***** unimplemented Arduino functions ******/
+
+// // Register Read/Write Commands
+// 
+// 
+// byte ADS1299::RREG(byte _address) {		//  reads ONE register at _address
+//     byte opcode1 = _address + 0x20; 	//  RREG expects 001rrrrr where rrrrr = _address
+//     digitalWrite(CS, LOW); 				//  open SPI
+//     transfer(opcode1); 					//  opcode1
+//     transfer(0x00); 					//  opcode2
+//     regData[_address] = transfer(0x00);//  update mirror location with returned byte
+//     digitalWrite(CS, HIGH); 			//  close SPI	
+// 	if (verbose){						//  verbose output
+// 		printRegisterName(_address);
+// 		printHex(_address);
+// 		Serial.print(", ");
+// 		printHex(regData[_address]);
+// 		Serial.print(", ");
+// 		for(byte j = 0; j<8; j++){
+// 			Serial.print(bitRead(regData[_address], 7-j));
+// 			if(j!=7) Serial.print(", ");
+// 		}
+// 		
+// 		Serial.println();
+// 	}
+// 	return regData[_address];			// return requested register value
+// }
+// 
+// // Read more than one register starting at _address
+// void ADS1299::RREGS(byte _address, byte _numRegistersMinusOne) {
+// //	for(byte i = 0; i < 0x17; i++){
+// //		regData[i] = 0;					//  reset the regData array
+// //	}
+//     byte opcode1 = _address + 0x20; 	//  RREG expects 001rrrrr where rrrrr = _address
+//     digitalWrite(CS, LOW); 				//  open SPI
+//     transfer(opcode1); 					//  opcode1
+//     transfer(_numRegistersMinusOne);	//  opcode2
+//     for(int i = 0; i <= _numRegistersMinusOne; i++){
+//         regData[_address + i] = transfer(0x00); 	//  add register byte to mirror array
+// 		}
+//     digitalWrite(CS, HIGH); 			//  close SPI
+// 	if(verbose){						//  verbose output
+// 		for(int i = 0; i<= _numRegistersMinusOne; i++){
+// 			printRegisterName(_address + i);
+// 			printHex(_address + i);
+// 			Serial.print(", ");
+// 			printHex(regData[_address + i]);
+// 			Serial.print(", ");
+// 			for(int j = 0; j<8; j++){
+// 				Serial.print(bitRead(regData[_address + i], 7-j));
+// 				if(j!=7) Serial.print(", ");
+// 			}
+// 			Serial.println();
+// 		}
+//     }
+//     
+// }
+// 
+// void ADS1299::WREG(byte _address, byte _value) {	//  Write ONE register at _address
+//     byte opcode1 = _address + 0x40; 	//  WREG expects 010rrrrr where rrrrr = _address
+//     digitalWrite(CS, LOW); 				//  open SPI
+//     transfer(opcode1);					//  Send WREG command & address
+//     transfer(0x00);						//	Send number of registers to read -1
+//     transfer(_value);					//  Write the value to the register
+//     digitalWrite(CS, HIGH); 			//  close SPI
+// 	regData[_address] = _value;			//  update the mirror array
+// 	if(verbose){						//  verbose output
+// 		Serial.print(F("Register "));
+// 		printHex(_address);
+// 		Serial.println(F(" modified."));
+// 	}
+// }
+// 
+// void ADS1299::WREGS(byte _address, byte _numRegistersMinusOne) {
+//     byte opcode1 = _address + 0x40;		//  WREG expects 010rrrrr where rrrrr = _address
+//     digitalWrite(CS, LOW); 				//  open SPI
+//     transfer(opcode1);					//  Send WREG command & address
+//     transfer(_numRegistersMinusOne);	//	Send number of registers to read -1	
+// 	for (int i=_address; i <=(_address + _numRegistersMinusOne); i++){
+// 		transfer(regData[i]);			//  Write to the registers
+// 	}	
+// 	digitalWrite(CS,HIGH);				//  close SPI
+// 	if(verbose){
+// 		Serial.print(F("Registers "));
+// 		printHex(_address); Serial.print(F(" to "));
+// 		printHex(_address + _numRegistersMinusOne);
+// 		Serial.println(F(" modified"));
+// 	}
+// }
+// 
+// 
+// void ADS1299::updateChannelData(){
+// 	byte inByte;
+// 	int nchan=8;  //assume 8 channel.  If needed, it automatically changes to 16 automatically in a later block.
+// 	digitalWrite(CS, LOW);				//  open SPI
+// 	
+// 	// READ CHANNEL DATA FROM FIRST ADS IN DAISY LINE
+// 	for(int i=0; i<3; i++){			//  read 3 byte status register from ADS 1 (1100+LOFF_STATP+LOFF_STATN+GPIO[7:4])
+// 		inByte = transfer(0x00);
+// 		stat_1 = (stat_1<<8) | inByte;				
+// 	}
+// 	
+// 	for(int i = 0; i<8; i++){
+// 		for(int j=0; j<3; j++){		//  read 24 bits of channel data from 1st ADS in 8 3 byte chunks
+// 			inByte = transfer(0x00);
+// 			channelData[i] = (channelData[i]<<8) | inByte;
+// 		}
+// 	}
+// 	
+// 	if (isDaisy) {
+// 		nchan = 16;
+// 		// READ CHANNEL DATA FROM SECOND ADS IN DAISY LINE
+// 		for(int i=0; i<3; i++){			//  read 3 byte status register from ADS 2 (1100+LOFF_STATP+LOFF_STATN+GPIO[7:4])
+// 			inByte = transfer(0x00);
+// 			stat_2 = (stat_1<<8) | inByte;				
+// 		}
+// 		
+// 		for(int i = 8; i<16; i++){
+// 			for(int j=0; j<3; j++){		//  read 24 bits of channel data from 2nd ADS in 8 3 byte chunks
+// 				inByte = transfer(0x00);
+// 				channelData[i] = (channelData[i]<<8) | inByte;
+// 			}
+// 		}
+// 	}
+// 	
+// 	digitalWrite(CS, HIGH);				//  close SPI
+// 	
+// 	//reformat the numbers
+// 	for(int i=0; i<nchan; i++){			// convert 3 byte 2's compliment to 4 byte 2's compliment	
+// 		if(bitRead(channelData[i],23) == 1){	
+// 			channelData[i] |= 0xFF000000;
+// 		}else{
+// 			channelData[i] &= 0x00FFFFFF;
+// 		}
+// 	}
+// }
+// 
+// 	
+// //read data
+// void ADS1299::RDATA() {				//  use in Stop Read Continuous mode when DRDY goes low
+// 	byte inByte;
+// 	stat_1 = 0;							//  clear the status registers
+// 	stat_2 = 0;	
+// 	int nchan = 8;	//assume 8 channel.  If needed, it automatically changes to 16 automatically in a later block.
+// 	digitalWrite(CS, LOW);				//  open SPI
+// 	transfer(_RDATA);
+// 	
+// 	// READ CHANNEL DATA FROM FIRST ADS IN DAISY LINE
+// 	for(int i=0; i<3; i++){			//  read 3 byte status register (1100+LOFF_STATP+LOFF_STATN+GPIO[7:4])
+// 		inByte = transfer(0x00);
+// 		stat_1 = (stat_1<<8) | inByte;				
+// 	}
+// 	
+// 	for(int i = 0; i<8; i++){
+// 		for(int j=0; j<3; j++){		//  read 24 bits of channel data from 1st ADS in 8 3 byte chunks
+// 			inByte = transfer(0x00);
+// 			channelData[i] = (channelData[i]<<8) | inByte;
+// 		}
+// 	}
+// 	
+// 	if (isDaisy) {
+// 		nchan = 16;
+// 		
+// 		// READ CHANNEL DATA FROM SECOND ADS IN DAISY LINE
+// 		for(int i=0; i<3; i++){			//  read 3 byte status register (1100+LOFF_STATP+LOFF_STATN+GPIO[7:4])
+// 			inByte = transfer(0x00);
+// 			stat_2 = (stat_1<<8) | inByte;				
+// 		}
+// 		
+// 		for(int i = 8; i<16; i++){
+// 			for(int j=0; j<3; j++){		//  read 24 bits of channel data from 2nd ADS in 8 3 byte chunks
+// 				inByte = transfer(0x00);
+// 				channelData[i] = (channelData[i]<<8) | inByte;
+// 			}
+// 		}
+// 	}
+// 	
+// 	for(int i=0; i<nchan; i++){			// convert 3 byte 2's compliment to 4 byte 2's compliment	
+// 		if(bitRead(channelData[i],23) == 1){	
+// 			channelData[i] |= 0xFF000000;
+// 		}else{
+// 			channelData[i] &= 0x00FFFFFF;
+// 		}
+// 	}
+// 	
+//     
+// }
+// 
+// 
+// 
+// // String-Byte converters for RREG and WREG
+// void ADS1299::printRegisterName(byte _address) {
+//     if(_address == ID){
+//         Serial.print(F("ID, ")); //the "F" macro loads the string directly from Flash memory, thereby saving RAM
+//     }
+//     else if(_address == CONFIG1){
+//         Serial.print(F("CONFIG1, "));
+//     }
+//     else if(_address == CONFIG2){
+//         Serial.print(F("CONFIG2, "));
+//     }
+//     else if(_address == CONFIG3){
+//         Serial.print(F("CONFIG3, "));
+//     }
+//     else if(_address == LOFF){
+//         Serial.print(F("LOFF, "));
+//     }
+//     else if(_address == CH1SET){
+//         Serial.print(F("CH1SET, "));
+//     }
+//     else if(_address == CH2SET){
+//         Serial.print(F("CH2SET, "));
+//     }
+//     else if(_address == CH3SET){
+//         Serial.print(F("CH3SET, "));
+//     }
+//     else if(_address == CH4SET){
+//         Serial.print(F("CH4SET, "));
+//     }
+//     else if(_address == CH5SET){
+//         Serial.print(F("CH5SET, "));
+//     }
+//     else if(_address == CH6SET){
+//         Serial.print(F("CH6SET, "));
+//     }
+//     else if(_address == CH7SET){
+//         Serial.print(F("CH7SET, "));
+//     }
+//     else if(_address == CH8SET){
+//         Serial.print(F("CH8SET, "));
+//     }
+//     else if(_address == BIAS_SENSP){
+//         Serial.print(F("BIAS_SENSP, "));
+//     }
+//     else if(_address == BIAS_SENSN){
+//         Serial.print(F("BIAS_SENSN, "));
+//     }
+//     else if(_address == LOFF_SENSP){
+//         Serial.print(F("LOFF_SENSP, "));
+//     }
+//     else if(_address == LOFF_SENSN){
+//         Serial.print(F("LOFF_SENSN, "));
+//     }
+//     else if(_address == LOFF_FLIP){
+//         Serial.print(F("LOFF_FLIP, "));
+//     }
+//     else if(_address == LOFF_STATP){
+//         Serial.print(F("LOFF_STATP, "));
+//     }
+//     else if(_address == LOFF_STATN){
+//         Serial.print(F("LOFF_STATN, "));
+//     }
+//     else if(_address == GPIO){
+//         Serial.print(F("GPIO, "));
+//     }
+//     else if(_address == MISC1){
+//         Serial.print(F("MISC1, "));
+//     }
+//     else if(_address == MISC2){
+//         Serial.print(F("MISC2, "));
+//     }
+//     else if(_address == CONFIG4){
+//         Serial.print(F("CONFIG4, "));
+//     }
+// }
+// 
+// //SPI communication methods
+// byte ADS1299::transfer(byte _data) {
+// 	cli();
+//     SPDR = _data;
+//     while (!(SPSR & _BV(SPIF)))
+//         ;
+// 	sei();
+//     return SPDR;
+// }
+// 
+// // Used for printing HEX in verbose feedback mode
+// void ADS1299::printHex(byte _data){
+// 	Serial.print("0x");
+//     if(_data < 0x10) Serial.print("0");
+//     Serial.print(_data, HEX);
+// }
+
+/**** end unimplemented Arduino functions ****/
 
 function use (hardware, callback) {
   if(!hardware) {
--- test.js
+++ test.js
@@ -3,9 +3,11 @@ var BCILib = require('./');
 var BCI = BCILib.use(tessel.port['A']);
 
 BCI._reset(function () {
+  console.log('reset')
   BCI._start(function () {
+    console.log('started')
     BCI._rdatac(function () {
-      
+      console.log('did something')
     });
   });
 });
